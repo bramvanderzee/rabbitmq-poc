22f6fc1d43743e5839322657fc80f1e5a7868558
bramvanderzee <bram.vdzee@gmail.com>
2023-01-12 13:26:45 +0100

    added k6 test script

---
+++ k6test.js
@@ -0,0 +1,25 @@
+import { sleep } from 'k6'
+import http from 'k6/http'
+
+// See https://k6.io/docs/using-k6/options
+export const options = {
+  stages: [
+    { duration: '1m', target: 20 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.02'], // http errors should be less than 2%
+    http_req_duration: ['p(95)<1000'], // 95% requests should be below 2s
+  },
+  ext: {
+    loadimpact: {
+      distribution: {
+        'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 },
+      },
+    },
+  },
+}
+
+export default function main() {
+  let response = http.post('http://localhost:3000/k6-test', JSON.stringify({test: "hello"}), {})
+  sleep(0.5)
+}
